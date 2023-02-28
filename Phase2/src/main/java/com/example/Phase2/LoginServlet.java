8a743f27f046952133c698973cf0199c5ce9dd49
Özge Akkol <ozgeakkol.oa@gmail.com>
2023-02-28 22:39:17 +0300

    add login check from db to LoginServlet

--- Phase2/src/main/java/com/example/Phase2/LoginServlet.java
+++ Phase2/src/main/java/com/example/Phase2/LoginServlet.java
@@ -1,27 +1,35 @@
 package com.example.Phase2;
 
+import dao.LoginDao;
+
 import java.io.*;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
-import javax.servlet.annotation.*;
 
 public class LoginServlet extends HttpServlet {
-
 
     private static final long serialVersionUID = 1L;
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LoginDao loginDao = new LoginDao();
+
         String email = request.getParameter("email");
         String password = request.getParameter("password");
 
-        if (email.equals("admin@domain.com") && password.equals("123")) {
-            request.setAttribute("email", email);
-            request.getRequestDispatcher("home.jsp").forward(request, response);
-        } else {
-            request.setAttribute("errorMessage", "Invalid username or password.");
+        if(loginDao.checkUserEmail(email)){
+            if(loginDao.checkUserPassword(email, password)){
+                request.setAttribute("email", email);
+                request.getRequestDispatcher("home.jsp").forward(request, response);
+            }
+            else{
+                request.setAttribute("errorMessage", "Invalid password!");
+                request.getRequestDispatcher("login.jsp").forward(request, response);
+            }
+        }else{
+            request.setAttribute("errorMessage", "Invalid email!");
             request.getRequestDispatcher("login.jsp").forward(request, response);
         }
+
     }
 
     public void destroy() {
