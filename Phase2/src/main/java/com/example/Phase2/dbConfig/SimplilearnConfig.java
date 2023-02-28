bc961c1840881f87353454687b7a8b00d22f57d9
Özge Akkol <ozgeakkol.oa@gmail.com>
2023-02-28 22:37:37 +0300

    add hibernate configuration

---
+++ Phase2/src/main/java/com/example/Phase2/dbConfig/SimplilearnConfig.java
@@ -0,0 +1,12 @@
+package com.example.Phase2.dbConfig;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SimplilearnConfig {
+    public static SessionFactory getSessionFactory() {
+        Configuration con = new Configuration();
+        con.configure("hibernate.cfg.xml");
+        return con.buildSessionFactory();
+    }
+}
