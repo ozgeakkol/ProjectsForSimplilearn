f9d0d7a66c7aad07ff02532c6f90a2a5262c8ee5
Özge Akkol <ozgeakkol.oa@gmail.com>
2023-04-17 21:50:56 +0300

    add jpa dependencies

--- Phase3/src/main/java/com/example/sportyShoes/SportyShoesApplication.java
+++ Phase3/src/main/java/com/example/sportyShoes/SportyShoesApplication.java
@@ -2,8 +2,12 @@ package com.example.sportyShoes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.sportyShoes.repository")
+@EntityScan(basePackages = "com.example.sportyShoes.model")
 public class SportyShoesApplication {
 
 	public static void main(String[] args) {
