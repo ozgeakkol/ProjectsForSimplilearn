6d3c02389a1401ff0656c05ec861ad61e462c4f4
Özge Akkol <ozgeakkol.oa@gmail.com>
2023-04-26 22:05:48 +0300

    wrong controller annotation changed new endpoints added

--- Phase3/src/main/java/com/example/sportyShoes/controller/CategoryController.java
+++ Phase3/src/main/java/com/example/sportyShoes/controller/CategoryController.java
@@ -1,24 +1,30 @@
 package com.example.sportyShoes.controller;
 
 import com.example.sportyShoes.model.Category;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-
-@RestController
+@Controller
 public class CategoryController {
 
     @RequestMapping(value = "/addCategoryPage", method = RequestMethod.GET)
-    public String addCategoryPage(Model model){
+    public String addCategoryPage(Model model) {
         model.addAttribute("category", new Category());
-        //TODO: addCategoryPage does not loaded!
         return "addCategoryPage";
     }
 
+    @RequestMapping(value = "/viewCategoryPage", method = RequestMethod.GET)
+    public String viewCategoryPage(Model model) {
+        model.addAttribute("category", new Category());
+        model.addAttribute("categoryInfo", "there isn'T any category");
+        return "viewCategoryPage";
+    }
+
     @RequestMapping(value = "/addCategory", method = RequestMethod.POST)
-    public String addCategory(Category category, Model model){
+    public String addCategory(@ModelAttribute Category category, Model model){
         return "index";
     }
 }
